6f4d810f6786e0bbd983c32fa8700cf6c4665f33
SHLEE\dkcns <seunghyo_lee@daekyo.co.kr>
2023-10-04 17:24:32 +0900

    first commit

---
+++ src/test/java/com/islol/hyopgg/HyopggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.islol.hyopgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyopggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
